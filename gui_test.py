172e0d62ce5e20dba3d3c76a167a8b70c585509e
Patrik Dennis <patrik.franzen.dennis@gmail.com>
2025-06-12 01:58:07 +0200

    feat: add move and delete polygon

--- gui_test.py
+++ gui_test.py
@@ -4,164 +4,199 @@ from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QToolBar, QMenu,
     QToolButton, QWidget, QVBoxLayout, QSizePolicy
 )
-from PyQt6.QtGui import QAction, QFont
+from PyQt6.QtGui import QAction, QFont, QCursor
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 
 class MainWindow(QMainWindow):
-    CLOSE_THRESHOLD = 0.5  # distance threshold in data coords to close polygon
+    CLOSE_THRESHOLD = 0.5  # threshold to detect proximity
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Empty Window with Navigation Bar")
         self.resize(800, 600)
 
-        # Define larger font
-        font = QFont()
-        font.setPointSize(14)
+        # Toolbar setup
+        font = QFont(); font.setPointSize(14)
+        toolbar = QToolBar("Navigation"); toolbar.setFont(font)
+        self.addToolBar(toolbar)
 
-        # Navigation toolbar
-        nav_bar = QToolBar("Navigation")
-        nav_bar.setFont(font)
-        self.addToolBar(nav_bar)
-
-        # Mesh button with submenu
-        mesh_button = QToolButton(self)
-        mesh_button.setText("Mesh")
-        mesh_button.setFont(font)
-        mesh_menu = QMenu(self)
-        mesh_menu.setFont(font)
-
-        # Mesh submenu actions
-        generate_action = QAction("Generate Mesh", self)
-        refine_action = QAction("Refine Mesh", self)
-        mesh_menu.addAction(generate_action)
-        mesh_menu.addAction(refine_action)
-
-        # Draw submenu
-        draw_menu = mesh_menu.addMenu("Draw")
-        draw_menu.setFont(font)
-        polygon_action = QAction("Polygon", self)
-        draw_menu.addAction(polygon_action)
+        mesh_btn = QToolButton(self)
+        mesh_btn.setText("Mesh"); mesh_btn.setFont(font)
+        mesh_menu = QMenu(self); mesh_menu.setFont(font)
+        gen_act = QAction("Generate Mesh", self)
+        ref_act = QAction("Refine Mesh", self)
+        mesh_menu.addAction(gen_act); mesh_menu.addAction(ref_act)
+        draw_menu = mesh_menu.addMenu("Draw"); draw_menu.setFont(font)
+        poly_act = QAction("Polygon", self); draw_menu.addAction(poly_act)
+        mesh_btn.setMenu(mesh_menu)
+        mesh_btn.setPopupMode(QToolButton.ToolButtonPopupMode.InstantPopup)
+        toolbar.addWidget(mesh_btn)
 
         # Connect actions
-        generate_action.triggered.connect(self.on_generate_mesh)
-        refine_action.triggered.connect(self.on_refine_mesh)
-        polygon_action.triggered.connect(self.on_draw_polygon)
-
-        mesh_button.setMenu(mesh_menu)
-        mesh_button.setPopupMode(QToolButton.ToolButtonPopupMode.InstantPopup)
-        nav_bar.addWidget(mesh_button)
+        gen_act.triggered.connect(self.on_generate_mesh)
+        ref_act.triggered.connect(self.on_refine_mesh)
+        poly_act.triggered.connect(self.on_draw_polygon)
 
         # Central widget
-        self.container = QWidget()
-        self.setCentralWidget(self.container)
+        self.container = QWidget(); self.setCentralWidget(self.container)
         self.layout = QVBoxLayout(self.container)
 
-        # Drawing state
+        # State
         self.canvas = None
-        self.toolbar = None
-        self.poly_points = []         # current polygon vertices
-        self.poly_artists = []        # artists for current polygon
-        self.permanent_artists = []   # artists for closed polygons
-        self.drawing = False          # whether drawing mode is active
-
-    def zoom_callback(self, event):
-        ax = event.inaxes
-        if ax is None:
-            return
-        base_scale = 1.1
-        scale_factor = base_scale if event.button == 'up' else 1/base_scale
-        xdata, ydata = event.xdata, event.ydata
-        xlim = ax.get_xlim(); ylim = ax.get_ylim()
-        new_width = (xlim[1] - xlim[0]) * scale_factor
-        new_height = (ylim[1] - ylim[0]) * scale_factor
-        relx = (xdata - xlim[0])/(xlim[1] - xlim[0])
-        rely = (ydata - ylim[0])/(ylim[1] - ylim[0])
-        ax.set_xlim([xdata - new_width*relx, xdata + new_width*(1-relx)])
-        ax.set_ylim([ydata - new_height*rely, ydata + new_height*(1-rely)])
-        self.canvas.draw_idle()
+        self.drawing = False
+        self.current_points = []
+        self.current_artists = []
+        self.polygons = []  # each: {'points':..., 'artists':...}
+        self.selected_idx = None
+        self.mode = None
+        self.dragging = False
+        self.last_mouse = None
 
     def on_generate_mesh(self):
-        if self.canvas is None:
+        if not self.canvas:
             fig, ax = plt.subplots()
             ax.axhline(0, color='black', linewidth=2)
             ax.axvline(0, color='black', linewidth=2)
-            ax.set_aspect('equal', 'box')
-            ax.set_xlim(-10, 10); ax.set_ylim(-10, 10)
+            ax.set_aspect('equal', 'box'); ax.set_xlim(-10, 10); ax.set_ylim(-10, 10)
 
             self.canvas = FigureCanvas(fig)
             self.canvas.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-            self.canvas.updateGeometry()
-            fig.canvas.mpl_connect('scroll_event', self.zoom_callback)
             fig.canvas.mpl_connect('button_press_event', self.on_click)
+            fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
+            fig.canvas.mpl_connect('button_release_event', self.on_release)
 
-            self.toolbar = NavigationToolbar(self.canvas, self)
-            self.toolbar.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-            self.layout.addWidget(self.toolbar)
+            nav = NavigationToolbar(self.canvas, self)
+            self.layout.addWidget(nav);
             self.layout.addWidget(self.canvas)
-        else:
-            ax = self.canvas.figure.axes[0]
-            ax.cla(); ax.axhline(0, color='black', linewidth=2)
-            ax.axvline(0, color='black', linewidth=2)
-            ax.set_aspect('equal', 'box'); ax.set_xlim(-10, 10); ax.set_ylim(-10, 10)
-            # clear current drawing
-            self.poly_points.clear()
-            for artist in self.poly_artists:
-                artist.remove()
-            self.poly_artists.clear()
-            self.canvas.draw()
-        self.drawing = True
-        print("Mesh generated. Drawing mode ready.")
+
+        ax = self.canvas.figure.axes[0]
+        ax.cla()
+        ax.axhline(0, color='black', linewidth=2)
+        ax.axvline(0, color='black', linewidth=2)
+        ax.set_aspect('equal', 'box'); ax.set_xlim(-10, 10); ax.set_ylim(-10, 10)
+
+        # Clear polygons
+        for poly in self.polygons:
+            for art in poly['artists']:
+                try: art.remove()
+                except: pass
+        self.polygons.clear()
+        self.clear_current()
+        self.selected_idx = None; self.mode = None
+        self.canvas.draw()
+        print("Mesh generated.")
 
     def on_draw_polygon(self):
-        if self.canvas is None:
+        if not self.canvas:
             self.on_generate_mesh()
-        # start new polygon: clear current points only
-        self.poly_points.clear()
-        self.poly_artists.clear()
         self.drawing = True
-        print("Polygon draw mode: start clicking vertices.")
-
-    def on_click(self, event):
-        if not self.drawing: return
-        ax = event.inaxes
-        if ax is None or event.button != 1: return
-        x, y = event.xdata, event.ydata
-        # close if near first and enough points
-        if len(self.poly_points) >= 3:
-            x0, y0 = self.poly_points[0]
-            if hypot(x-x0, y-y0) <= self.CLOSE_THRESHOLD:
-                self.poly_points.append((x0, y0))
-                self._draw_polygon(ax)
-                # finalize polygon
-                self.permanent_artists.extend(self.poly_artists)
-                self.poly_artists.clear()
-                self.poly_points.clear()
-                self.drawing = False
-                print("Polygon closed.")
-                return
-        # add new point
-        self.poly_points.append((x, y))
-        self._draw_polygon(ax)
-
-    def _draw_polygon(self, ax):
-        # remove only current artists
-        for artist in self.poly_artists:
-            artist.remove()
-        self.poly_artists.clear()
-        xs, ys = zip(*self.poly_points)
-        scatter = ax.scatter(xs, ys, c='blue')
-        line, = ax.plot(xs, ys, linestyle='-', marker='o', color='blue')
-        self.poly_artists.extend([scatter, line])
-        self.canvas.draw()
+        self.clear_current()
+        self.selected_idx = None
+        self.redraw_polygons()
+        print("Polygon draw mode activated.")
 
     def on_refine_mesh(self):
         print("Refine Mesh selected")
 
+    def on_click(self, event):
+        ax = event.inaxes
+        if not ax: return
+        # Draw new polygon
+        if self.drawing and event.button == 1:
+            x, y = event.xdata, event.ydata
+            if len(self.current_points) >= 3 and hypot(x-self.current_points[0][0], y-self.current_points[0][1]) <= self.CLOSE_THRESHOLD:
+                # close
+                self.current_points.append(self.current_points[0])
+                self.draw_current()
+                self.polygons.append({'points': list(self.current_points), 'artists': list(self.current_artists)})
+                self.clear_current(); self.drawing = False
+                print("Polygon closed.")
+            else:
+                self.current_points.append((x, y)); self.draw_current()
+            return
+        # Context menu
+        if not self.drawing and event.button == 3:
+            for idx, poly in enumerate(self.polygons):
+                for px, py in poly['points']:
+                    if hypot(event.xdata-px, event.ydata-py) <= self.CLOSE_THRESHOLD:
+                        self.selected_idx = idx
+                        self.show_context_menu(event)
+                        return
+
+    def show_context_menu(self, event):
+        menu = QMenu(self)
+        move = QAction("Move", self); mod = QAction("Modify", self); dele = QAction("Delete", self)
+        menu.addAction(move); menu.addAction(mod); menu.addAction(dele)
+        move.triggered.connect(lambda: self.start_move(event))
+        mod.triggered.connect(self.start_modify)
+        dele.triggered.connect(self.start_delete)
+        menu.exec(QCursor.pos())
+
+    def start_move(self, event):
+        self.mode = 'move'; self.dragging = True
+        self.last_mouse = (event.xdata, event.ydata)
+        print("Move mode activated.")
+
+    def start_modify(self):
+        self.mode = 'modify'
+        print("Modify mode activated.")
+
+    def start_delete(self):
+        idx = self.selected_idx
+        if idx is not None:
+            for art in self.polygons[idx]['artists']:
+                try: art.remove()
+                except: pass
+            self.polygons.pop(idx)
+            self.selected_idx = None
+            self.redraw_polygons()
+
+    def draw_current(self):
+        self.clear_current_artists()
+        ax = self.canvas.figure.axes[0]
+        xs, ys = zip(*self.current_points)
+        sc = ax.scatter(xs, ys, c='blue'); ln, = ax.plot(xs, ys, marker='o', c='blue')
+        self.current_artists = [sc, ln]; self.canvas.draw()
+
+    def clear_current(self):
+        self.current_points.clear(); self.clear_current_artists()
+
+    def clear_current_artists(self):
+        for art in self.current_artists:
+            try: art.remove()
+            except: pass
+        self.current_artists.clear()
+
+    def redraw_polygons(self):
+        ax = self.canvas.figure.axes[0]
+        ax.cla()
+        ax.axhline(0, color='black', linewidth=2)
+        ax.axvline(0, color='black', linewidth=2)
+        ax.set_aspect('equal', 'box'); ax.set_xlim(-10, 10); ax.set_ylim(-10, 10)
+        for idx, poly in enumerate(self.polygons):
+            pts = poly['points']; xs, ys = zip(*pts)
+            color = 'green' if idx == self.selected_idx else 'blue'
+            sc = ax.scatter(xs, ys, c=color); ln, = ax.plot(xs, ys, marker='o', c=color)
+            poly['artists'] = [sc, ln]
+        self.canvas.draw()
+
+    def on_motion(self, event):
+        if not self.dragging or self.mode != 'move' or self.selected_idx is None:
+            return
+        ax = self.canvas.figure.axes[0]; x, y = event.xdata, event.ydata
+        dx = x - self.last_mouse[0]; dy = y - self.last_mouse[1]
+        self.last_mouse = (x, y)
+        poly = self.polygons[self.selected_idx]
+        pts = [(px+dx, py+dy) for px, py in poly['points']]
+        poly['points'] = pts
+        self.redraw_polygons()
+
+    def on_release(self, event):
+        if self.dragging:
+            self.dragging = False; self.mode = None; self.last_mouse = None
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    w = MainWindow(); w.show(); sys.exit(app.exec())
 
