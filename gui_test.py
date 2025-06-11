8ecec1e9e4b71e8a8df9776b015f6a93a6e99b5c
Patrik Dennis <patrik.franzen.dennis@gmail.com>
2025-06-12 01:34:20 +0200

    feat: add draw option

--- gui_test.py
+++ gui_test.py
@@ -31,12 +31,24 @@ class MainWindow(QMainWindow):
         mesh_button.setFont(font)
         mesh_menu = QMenu(self)
         mesh_menu.setFont(font)
+
+        # Mesh submenu actions
         generate_action = QAction("Generate Mesh", self)
         refine_action = QAction("Refine Mesh", self)
         mesh_menu.addAction(generate_action)
         mesh_menu.addAction(refine_action)
+
+        # Draw submenu
+        draw_menu = mesh_menu.addMenu("Draw")
+        draw_menu.setFont(font)
+        polygon_action = QAction("Polygon", self)
+        draw_menu.addAction(polygon_action)
+
+        # Connect actions
         generate_action.triggered.connect(self.on_generate_mesh)
         refine_action.triggered.connect(self.on_refine_mesh)
+        polygon_action.triggered.connect(self.on_draw_polygon)
+
         mesh_button.setMenu(mesh_menu)
         mesh_button.setPopupMode(QToolButton.ToolButtonPopupMode.InstantPopup)
         nav_bar.addWidget(mesh_button)
@@ -71,6 +83,7 @@ class MainWindow(QMainWindow):
         self.canvas.draw_idle()
 
     def on_generate_mesh(self):
+        # Create axes if needed
         if self.canvas is None:
             fig, ax = plt.subplots()
             ax.axhline(0, color='black', linewidth=2)
@@ -91,6 +104,7 @@ class MainWindow(QMainWindow):
             self.layout.addWidget(self.toolbar)
             self.layout.addWidget(self.canvas)
         else:
+            # Reset axes
             ax = self.canvas.figure.axes[0]
             ax.cla()
             ax.axhline(0, color='black', linewidth=2)
@@ -98,14 +112,29 @@ class MainWindow(QMainWindow):
             ax.set_aspect('equal', 'box')
             ax.set_xlim(-10, 10)
             ax.set_ylim(-10, 10)
+            # Clear drawing state
             self.poly_points.clear()
             for artist in self.poly_artists:
                 artist.remove()
             self.poly_artists.clear()
             self.canvas.draw()
-        # Activate drawing mode
         self.drawing = True
-        print("Generated coordinate axes. Scroll to zoom, drag to pan. Click to add polygon points.")
+        print("Generated coordinate axes. Scroll to zoom, drag to pan. Ready to draw or refine.")
+
+    def on_draw_polygon(self):
+        # Start polygon drawing mode
+        if self.canvas is None:
+            self.on_generate_mesh()
+        else:
+            # Clear any previous polygon
+            ax = self.canvas.figure.axes[0]
+            for artist in self.poly_artists:
+                artist.remove()
+            self.poly_artists.clear()
+            self.poly_points.clear()
+            self.canvas.draw()
+        self.drawing = True
+        print("Polygon draw mode activated. Click to add points. Click near start to close.")
 
     def on_click(self, event):
         if not self.drawing:
@@ -114,17 +143,16 @@ class MainWindow(QMainWindow):
         if ax is None or event.button != 1:
             return
         x, y = event.xdata, event.ydata
-        # Close polygon if click near start and enough points
+        # Close polygon if near start and enough points
         if len(self.poly_points) >= 3:
             x0, y0 = self.poly_points[0]
             if hypot(x - x0, y - y0) <= self.CLOSE_THRESHOLD:
-                # Append starting point, draw closed polygon, then exit drawing mode
                 self.poly_points.append((x0, y0))
                 self._draw_polygon(ax)
                 self.drawing = False
                 print("Polygon closed. Returning to view mode.")
                 return
-        # Add new vertex
+        # Add vertex
         self.poly_points.append((x, y))
         self._draw_polygon(ax)
 
@@ -134,12 +162,9 @@ class MainWindow(QMainWindow):
             artist.remove()
         self.poly_artists.clear()
         xs, ys = zip(*self.poly_points)
-        # Scatter points
         scatter = ax.scatter(xs, ys, c='blue')
-        self.poly_artists.append(scatter)
-        # Plot connecting lines
         line, = ax.plot(xs, ys, linestyle='-', marker='o', color='blue')
-        self.poly_artists.append(line)
+        self.poly_artists.extend([scatter, line])
         self.canvas.draw()
 
     def on_refine_mesh(self):
