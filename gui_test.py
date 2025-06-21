b5450739f21bd48310b19e5fc682dd24bff226d2
Patrik Dennis <patrik.franzen.dennis@gmail.com>
2025-06-21 13:59:43 +0200

    fix: modification and movement window resizing

--- gui_test.py
+++ gui_test.py
@@ -2,9 +2,10 @@ import sys
 from math import hypot
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QToolBar, QMenu,
-    QToolButton, QWidget, QVBoxLayout, QSizePolicy
+    QToolButton, QWidget, QVBoxLayout, QSizePolicy, QMessageBox
 )
 from PyQt6.QtGui import QAction, QFont, QCursor
+from shapely.geometry import Polygon as ShapelyPoly
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 plt.style.use("dark_background")
@@ -52,7 +53,7 @@ class MainWindow(QMainWindow):
         # Connect actions
         gen_act.triggered.connect(self.on_generate_mesh)
         ref_act.triggered.connect(self.on_refine_mesh)
-        poly_act.triggered.connect(self.on_draw_polygon)
+        poly_act.triggered.connect(self.on_draw_shape)
         #circle_act.triggered.connect(self.on_draw_circle)
 
         # Central widget
@@ -65,7 +66,7 @@ class MainWindow(QMainWindow):
         self.drawing = False
         self.current_points = []
         self.current_artists = []
-        self.polygons = []  # list of dicts with 'points' and 'artists'
+        self.shapes = []  # list of dicts with 'points' and 'artists'
         self.circles = []
         self.selected_idx = None
         self.mode = None  # 'move' or 'modify'
@@ -101,29 +102,38 @@ class MainWindow(QMainWindow):
         ax.set_xlim(-10, 10)
         ax.set_ylim(-10, 10)
 
-        # Clear existing polygons
-        for poly in self.polygons:
+        # Clear existing shapes
+        for poly in self.shapes:
             for art in poly['artists']:
                 try:
                     art.remove()
                 except:
                     pass
-        self.polygons.clear()
+        self.shapes.clear()
         self.clear_current()
         self.selected_idx = None
         self.mode = None
         self.canvas.draw()
         print("Mesh generated.")
 
-    def on_draw_polygon(self):
+    def on_draw_shape(self):
         if not self.canvas:
             self.on_generate_mesh()
         self.drawing = True
         self.clear_current()
         self.selected_idx = None
-        self.redraw_polygons()
+        self.redraw_shapes()
         print("Polygon draw mode activated.")
 
+    # def on_draw_cirlcle(self):
+    #     if not self.canvas:
+    #         self.on_generate_mesh()
+    #     self.drawing = True
+    #     self.clear_current()
+    #     self.selected_idx = None
+    #     self.redraw_shapes()
+    #     print("Circle draw mode activated")
+        
     def zoom_callback(self, event):
         ax = event.inaxes
         if ax is None:
@@ -152,25 +162,28 @@ class MainWindow(QMainWindow):
         # Drawing mode: left-click
         if self.drawing and event.button == 1:
             x, y = event.xdata, event.ydata
+            
+            # want to change this so we have pixel threshold instead of distance!!!!
             if len(self.current_points) >= 3 and hypot(x - self.current_points[0][0], y - self.current_points[0][1]) <= self.CLOSE_THRESHOLD:
                 # Close polygon
                 self.current_points.append(self.current_points[0])
                 self.draw_current()
-                self.polygons.append({
+                self.shapes.append({
                     'points': list(self.current_points),
                     'artists': list(self.current_artists)
                 })
                 self.clear_current()
                 self.drawing = False
-                self.redraw_polygons()
+                self.redraw_shapes()
                 print("Polygon closed.")
             else:
                 self.current_points.append((x, y))
                 self.draw_current()
             return
         # Context menu: right-click
-        if not self.drawing and event.button == 3:
-            for idx, poly in enumerate(self.polygons):
+        #if not self.drawing and event.button == 3:
+        if event.button == 3:
+            for idx, poly in enumerate(self.shapes):
                 for px, py in poly['points']:
                     if hypot(event.xdata - px, event.ydata - py) <= self.CLOSE_THRESHOLD:
                         self.selected_idx = idx
@@ -202,7 +215,7 @@ class MainWindow(QMainWindow):
     def start_modify(self):
         if self.context_event and self.selected_idx is not None:
             x0, y0 = self.context_event.xdata, self.context_event.ydata
-            points = self.polygons[self.selected_idx]['points']
+            points = self.shapes[self.selected_idx]['points']
             dists = [hypot(px - x0, py - y0) for px, py in points]
             self.modify_vidx = dists.index(min(dists))
             self.mode = 'modify'
@@ -215,22 +228,54 @@ class MainWindow(QMainWindow):
     def start_delete(self):
         idx = self.selected_idx
         if idx is not None:
-            for art in self.polygons[idx]['artists']:
+            for art in self.shapes[idx]['artists']:
                 try:
                     art.remove()
                 except:
                     pass
-            self.polygons.pop(idx)
+            self.shapes.pop(idx)
             self.selected_idx = None
-            self.redraw_polygons()
+            self.redraw_shapes()
 
     def draw_current(self):
-        self.clear_current_artists()
+        #self.clear_current_artists()
+        #ax = self.canvas.figure.axes[0]
+        #xs, ys = zip(*self.current_points)
+        #sc = ax.scatter(xs, ys, c='blue')
+        #ln, = ax.plot(xs, ys, marker='o', c='blue')
+        #self.current_artists = [sc, ln]
+        #self.canvas.draw()
+
         ax = self.canvas.figure.axes[0]
-        xs, ys = zip(*self.current_points)
-        sc = ax.scatter(xs, ys, c='blue')
-        ln, = ax.plot(xs, ys, marker='o', c='blue')
-        self.current_artists = [sc, ln]
+        xlim, ylim = ax.get_xlim(), ax.get_ylim()
+        ax.cla()
+
+        ax.axhline(0, color='white', linewidth=1)
+        ax.axvline(0, color='white', linewidth=1)
+        ax.set_aspect('equal', 'box')
+        ax.set_xlim(*xlim)
+        ax.set_ylim(*ylim)
+
+        
+        # draw the “in-progress” polygon
+        #xs, ys = zip(*self.current_points)
+        #ax.scatter(xs, ys, c='blue')
+        #ax.plot(xs, ys, marker='o', c='blue')
+
+
+        # 4) draw *all* saved shapes
+        for idx, poly in enumerate(self.shapes):
+            xs, ys = zip(*poly['points'])
+            color = 'green' if idx == self.selected_idx else 'blue'
+            ax.scatter(xs, ys, c=color)
+            ax.plot(xs, ys, marker='o', c=color)
+
+        # 5) finally draw the in‐progress ones
+        if self.current_points:
+            xs, ys = zip(*self.current_points)
+            ax.scatter(xs, ys, c='blue')
+            ax.plot(xs, ys, marker='o', c='blue')
+
         self.canvas.draw()
 
     def clear_current(self):
@@ -250,14 +295,14 @@ class MainWindow(QMainWindow):
         Highlight the polygon at given index by setting it as selected and redrawing.
         """
         self.selected_idx = idx
-        self.redraw_polygons()
+        self.redraw_shapes()
 
     def unhighlight(self):
         """
         Clear any polygon selection.
         """
         self.selected_idx = None
-        self.redraw_polygons()
+        self.redraw_shapes()
 
         for art in self.current_artists:
             try:
@@ -266,15 +311,21 @@ class MainWindow(QMainWindow):
                 pass
         self.current_artists.clear()
 
-    def redraw_polygons(self):
+    def redraw_shapes(self):
+        #ax = self.canvas.figure.axes[0]
+        #ax.cla()
+
         ax = self.canvas.figure.axes[0]
+        xlim, ylim = ax.get_xlim(), ax.get_ylim()
         ax.cla()
-        ax.axhline(0, color='white', linewidth=2)
-        ax.axvline(0, color='white', linewidth=2)
+        ax.axhline(0, color='white', linewidth=1)
+        ax.axvline(0, color='white', linewidth=1)
         ax.set_aspect('equal', 'box')
-        ax.set_xlim(-10, 10)
-        ax.set_ylim(-10, 10)
-        for idx, poly in enumerate(self.polygons):
+        ax.set_xlim(*xlim)
+        ax.set_ylim(*ylim)
+        #ax.set_xlim(-10, 10)
+        #ax.set_ylim(-10, 10)
+        for idx, poly in enumerate(self.shapes):
             pts = poly['points']
             xs, ys = zip(*pts)
             color = 'green' if idx == self.selected_idx else 'blue'
@@ -283,6 +334,14 @@ class MainWindow(QMainWindow):
             poly['artists'] = [sc, ln]
         self.canvas.draw()
 
+    def polygons_overlap(pts, other_pts_list):
+        """Return True if `pts` overlaps/intersects any polygon in other_pts_list."""
+        p = ShapelyPoly(pts)
+        for o in other_pts_list:
+            if p.intersects(ShapelyPoly(o)):
+                return True
+        return False
+                
     def on_motion(self, event):
         if not self.dragging or self.selected_idx is None:
             return
@@ -299,7 +358,7 @@ class MainWindow(QMainWindow):
         dx = x - self.last_mouse[0] if self.last_mouse else 0
         dy = y - self.last_mouse[1] if self.last_mouse else 0
         self.last_mouse = (x, y)
-        poly = self.polygons[self.selected_idx]
+        poly = self.shapes[self.selected_idx]
         if self.mode == 'move':
             pts = [(px + dx, py + dy) for px, py in poly['points']]
             poly['points'] = pts
@@ -315,7 +374,7 @@ class MainWindow(QMainWindow):
             poly['points'] = pts
         else:
             return
-        self.redraw_polygons()
+        self.redraw_shapes()
 
     def on_release(self, event):
         if self.dragging:
